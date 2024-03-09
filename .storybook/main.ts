8503378a94bef42d1200793d46b4e5d99c6a2016
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-03-09 21:27:17 +0300

    storybook init

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+
+module.exports = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-onboarding',
+		'@storybook/addon-interactions',
+	],
+	framework: {
+		name: '@storybook/nextjs',
+		options: {},
+	},
+	docs: {
+		autodocs: 'tag',
+	},
+	webpackFinal: async (config) => {
+		config.resolve.modules.push(path.resolve(__dirname, '../src'));
+		return config;
+	},
+};
