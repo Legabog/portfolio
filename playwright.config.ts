28be11e71ca03b5c6be8c1e2924b00a5d2fa88f5
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-03-14 21:11:57 +0300

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run start',
 		url: process.env.TEST_ENV_BASE_URL,
-		reuseExistingServer: !process.env.CI,
+		reuseExistingServer: true,
 		stdout: 'ignore',
 		stderr: 'pipe',
 	},
