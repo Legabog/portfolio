3092fa361e14c7d59b033248f33bd1c8a753d661
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-03-16 22:10:20 +0300

    temp

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,30 @@
-import { FC } from 'react';
+'use client';
 
-const Home: FC = () => <div>HOME</div>;
+import { FC } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useSpring } from '@react-spring/core';
+import { a } from '@react-spring/web';
+
+import Sphere from 'src/feature/sphere/sphere';
+import { Wrapper } from './page.styled';
+
+const Home: FC = () => {
+	const [{ background }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, []);
+	return (
+		<Wrapper>
+			<a.main style={ { background } }>
+				<Canvas dpr={ [1, 2] }>
+					<Sphere setBg={ set } />
+					<OrbitControls
+						enablePan={ false }
+						enableZoom={ false }
+						maxPolarAngle={ Math.PI / 2 }
+						minPolarAngle={ Math.PI / 2 }
+					/>
+				</Canvas>
+			</a.main>
+		</Wrapper>
+	);
+};
 export default Home;
