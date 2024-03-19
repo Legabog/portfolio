f28f35df09584ae53d4af33c073668c17e9ae7ea
Oleg Dosov <user@24-003222.iteco-inno.ru>
2024-03-19 17:52:40 +0300

    fixed theme, sphere

--- src/app/global.styled.ts
+++ src/app/global.styled.ts
@@ -6,14 +6,13 @@ export const GlobalStyles = createGlobalStyle`
   }
   body {
     min-height: 100vh;
-		font-family: MaisonDemiDemi;
-		cursor: url('data:image/svg+xml;base64,${btoa('<svg width="24" height="24" viewBox="0 0 45 45" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.000244141 -0.0020752L17.5002 44.9971L22.5002 22.4975L45.0003 17.4976L0.000244141 -0.0020752Z" fill="#E8B059"/></svg>')}'), auto;
   }  
 	main {
-  	background: ${({ theme }) => theme.backgroundColor};
   	display: flex;
 		flex-direction: row;
 		min-height: 100vh;
+  	background: ${({ theme }) => theme.backgroundColor};
+		transition: background-color 0.5s;
 
 		@media only screen and (max-aspect-ratio: 8/7) {
     	flex-direction: column;
